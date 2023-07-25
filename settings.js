60c5fd2b0f99f11d172a673531c64f93780ed9c8
AriyanAhmed777 <140443808+AriyanAhmed777@users.noreply.github.com>
2023-07-25 11:17:44 +0500

    Add files via upload

--- settings.js
+++ settings.js
@@ -48,31 +48,31 @@ global.groupOnly = false //bot hanya bisa di gunakan dalam grup jika true
 //language
 //Available in indonesia , english & spanyol 
 //You can request others language 
-global.language = indonesia //change indonesia to english if you don't understand the language used by the bot
+global.language = english //change indonesia to english if you don't understand the language used by the bot
 //global.language = english // use this for English language
 //global.language = spanyol // use this for Spanyol language
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
+global.botname = "卂尺 ⃟ ⃟ ⃟乃ㄖㄒ⟣ᎷᎠ᭄ོ" //namabot kalian
+global.ownername = "Ａｒｉｙａｎོ ×፝֟͜×" //nama kalian
+global.myweb = "https://chrome" //bebas asal jan hapus
+global.youtube = "https://youtube.com/" //bebas asal jan hapus
 global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.timezone = 'Asia/Makassar' //  timezone wib
+global.email = "ariy175400@gmail.com" //bebas
+global.region = "Pakistan" //bebas
+global.timezone = 'Asia/Karachi' //  timezone wib
 global.premium_fitur = ["ai", "openai", "chatgpt"] //tambahin aja sendiri, soalnya gk semua org mau fiturnya premium jadi gw buat kyk gini aja, biar bisa custom fitur mana aja yang bakal di jadiin premium
-global.badword_list = ["asu", "anjir"] //tambahin sendiri kata² nya, gunakan huruf kecil
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62887435047326"] //ganti agar fitur owner bisa di gunakan
+global.badword_list = ["asu", "anjir","kanjar","fuck","bc","mc","chutiya","gandu","bsdk"] //tambahin sendiri kata² nya, gunakan huruf kecil
+global.premium = ['923555341506'] //premium user
+global.owner = ["923555341506"] //ganti agar fitur owner bisa di gunakan
 global.ownernomer = owner[0] + "@s.whatsapp.net"
 global.ownernomerr = "+"+ownernomer //tidak perlu di edit
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.tag_owner = "./tag_owner.mp3" // audio ini akan di kirim apabila ada member yang menandai (tag) owner bot 
-global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
+global.background_welcome = "https://i.ibb.co/KbFgLj5/Text-Pro-me-164bf683c2c21b.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.thumbGif = 'https://i.ibb.co/KbFgLj5/Text-Pro-me-164bf683c2c21b.jpg'
 global.packname = '© ' + botname //sticker wm ubah
 global.author = 'Created By ' + ownername //sticker wm ganti nama kalian
 global.sessionName = 'session'
